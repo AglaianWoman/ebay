54398a10f9180bc0e245dbacda7352b3fa75cb69
Devin Sarnacki <devin@sarnackidesign.com>
2016-01-13 14:11:13 -0600

    Ebay: Auction List js

--- build/jsx/main.jsx
+++ build/jsx/main.jsx
@@ -10,7 +10,7 @@ class AuctionList extends React.Component {
 	componentDidMount() {
 		let self = this;
 		$.ajax({
-			url: '//svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=' + this.props.apiKey + '&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=button&itemFilter(0).name=Seller&itemFilter(0).value(0)=' + this.props.sellerId,
+			url: '//svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=' + this.props.apiKey + '&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=' + encodeURI(this.props.keywords) + '&itemFilter(0).name=Seller&itemFilter(0).value(0)=' + this.props.sellerId,
 			dataType: 'jsonp'
 		}).done((data) => {
 			self.setState({
@@ -19,10 +19,21 @@ class AuctionList extends React.Component {
 		});
 	}
 	render() {
+		let items = this.state.items;
+		if (items.length > 1) items = items.map((item, i) => <AuctionItem key={i} item={item} />);
 		return (
-			<div></div>
+			<ul>{items}</ul>
 		);
 	}
 }
 
-ReactDOM.render(<AuctionList sellerId="IDHERE" apiKey="KEYHERE" />, document.getElementById('result'));
+class AuctionItem extends React.Component {
+	render() {
+		let price = (<span className="price">{'$' + parseFloat(this.props.item.sellingStatus[0].currentPrice[0].__value__).toFixed(2)}</span>);
+		return (
+			<li><a href={this.props.item.viewItemURL}><img src={this.props.item.galleryURL} alt={this.props.item.title} /><h3>{this.props.item.title}</h3> {price}</a></li>
+		);
+	}
+}
+
+ReactDOM.render(<AuctionList sellerId="SELLERID" apiKey="APIKEY" keywords="KEYWORDS"/>, document.getElementById('result'));
